8ee93d9d0adce5648b7f15cef9663c08015b79a5
jetson <EatingEdu>
2026-03-08 10:08:57 +0800

    test nwu

--- model_predict_forArm.py
+++ model_predict_forArm.py
@@ -101,6 +101,8 @@ npz_path = "./model/Miql_estimateF_MT_data4+6+7_envTTF/20750/actor_arm.npz" #这
 npz_path = "./model/0225_model0212Sim60k_data4_9_14_seg8_2_off08_sr08__envRanEFD__29125_output_arm_params/actor_arm.npz"
 npz_path = "./model/0225_model0212Sim60k_data4_9_14_seg8_2_off08_sr08__envRanEFDALLStep__48750_output_arm_params/actor_arm.npz"
 npz_path = "./model/modeldata8+9+10_envTTTrandomM41k__data9+11+12_envTTTrandomM15_offRatio05_sR05__13625_output_arm_params/actor_arm.npz"
+npz_path = "./model/data16_of05_sr02__envNoEFD_att15__27625_output_arm_params/actor_arm.npz"
+#npz_path = "./model/data16_of06_sr08__envNoEFD_att15__47000_output_arm_params/actor_arm.npz"
 
 predictor = ModelPredict(npz_path)
 dynamics_uav = Dynamics(thrust_to_weight=1/0.5)  
